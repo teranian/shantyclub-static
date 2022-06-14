47cab4ac9e65d9228b62f5e8fb4ecd2ff2fe9e9e
EwanL <ewan@funkymatic.net>
2022-06-14 20:15:49 +1000

    justify-center but working?

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@
 module.exports = function(eleventyConfig)  {
   eleventyConfig.addPassthroughCopy("./src/assets/img");
   eleventyConfig.addPassthroughCopy("./src/assets/css/main.css");
+  eleventyConfig.addPassthroughCopy("./src/assets/css/tailwind-3.1.1.css");
 
   // Tell 11ty to watch the Tailwind output file for changes
   eleventyConfig.addWatchTarget("./src/assets/css/main.css");
